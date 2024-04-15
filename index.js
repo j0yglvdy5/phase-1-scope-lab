18ffd10b4e7eecb13368e9f8e774c0eeafe1791f
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-04-15 08:04:23 +0300

    phase 1

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+
+function getCustomerName() {
+    return customerName;
+}
+customerName();
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+   let bestCustomer ='not bob'
+   return bestCustomer;
+}
+setBestCustomer();
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'someone else';
+    leastFavoriteCustomer = 'still someone else'; 
+}
+
+
+
+
